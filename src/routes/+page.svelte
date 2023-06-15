4c2929e3c55ccfffeb7b6ffc9a39196e585aa7c2
Rebeca vitoria <rebecanascimentovit@gmail.com>
2023-06-15 19:57:17 -0300

    página inicial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+    import Modal from "./modal.svelte";
+    let modal;
+</script>
+<body>
+    <div class="container">
+      <h1>CAÇA-PALAVRAS</h1>
+      <p>Bem-vindo! Divirta-se e desafie-se!</p>
+      <button><a href="/routes">iniciar jogo</a></button>
+      <button on:click={()=>modal.show()}>Informações</button>
+      <Modal bind:this={modal}>
+        <h2>informações</h2>
+        <p>Desenvolvido por: <br>Rebeca vitoria <br>Marilia gabriela <br>Arthur severino <br> Matheus josé <br>Glênio josé </p>
+       
+       
+        <button on:click={() => modal.hide()}>Fechar</button>
+      </Modal>
+  
+    
+       
+    </div>
+</body>
+       <style>
+         @font-face {
+      font-family: 'glossy';
+      src: url('Glossy Sheen Shine DEMO.ttf') format('truetype');
+      font-weight: normal;
+      font-style: normal;
+    }
+    .container {
+  text-align: center;
+  margin-top: 200px;
+  font-family: 'glossy';
+}
+
+h1 {
+  
+  font-size: 90px;
+  margin-bottom: 20px;
+  font-weight: normal;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color: rgb(71, 93, 153);
+  color: wheat;
+  border: 2px solid #0084ff;
+ border-style: inherit;
+  border-radius: 4px;
+  font-family: 'glossy';
+  }
+  button:hover{
+    box-shadow: 0 0 15px rgb(5, 198, 252), inset 0 0 15px rgb(4, 215, 252);
+  }
+a{
+  color: wheat;
+  text-decoration: none;
+ }
+body{
+  background-color: white;
+}
+</style>
---
+++ src/routes/modal.svelte
@@ -0,0 +1,42 @@
+<script>
+    let shown= false;
+   export function show(){
+        shown = true;
+ }
+ export function hide(){
+   shown = false;
+ }
+</script>
+<style>
+.modal-wrapper{
+  background-color: black;
+  background-color: rgba(0,0,0,0.6);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top:0;
+  left: 0;
+  
+}
+.modal{
+    background-color: whitesmoke;
+    max-width: 80vw;
+    padding: 1rem;
+    margin: 10% auto;
+
+}
+</style>
+<svelte:window
+on:keydown={ (e) => {
+    if(e.keyCode == 27){
+        hide();
+    }
+    }}/>
+
+{#if shown}
+<div class="modal-wrapper">
+ <div class="modal">
+     <slot />
+    </div>
+</div>
+{/if}
