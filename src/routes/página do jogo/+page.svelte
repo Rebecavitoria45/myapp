4e471b70279bd82def9539184dda1ea46b9ce24d
Rebeca vitoria <rebecanascimentovit@gmail.com>
2023-06-18 16:06:37 -0300

    página do jogo

---
+++ src/routes/página do jogo/+page.svelte
@@ -0,0 +1,219 @@
+<style>
+    td{
+        border: 2px solid black;
+        width: 60px;
+        height: 50px;
+        text-align: center;
+       color: white;
+       
+    }
+    .palavracruzada{
+        align-items: center;
+        display: flex;
+        justify-content: center;
+        
+        
+    }
+    table{
+        border-collapse: collapse;
+    }
+    td:empty{
+     background: black;
+    }
+.vertical{
+margin-left: 30px;
+margin-top: 60px;
+text-align: left;
+font-family: 'Times New Roman', Times, serif
+
+}
+.horizontal{
+margin-right: 50px;
+margin-top: 60px;
+text-align: left;
+font-family: 'Times New Roman', Times, serif;
+}
+@font-face {
+font-family: 'glossy';
+src: url('Glossy Sheen Shine DEMO.ttf') format('truetype');
+font-weight: normal;
+font-style: normal;
+}
+h2{
+font-family: 'glossy';
+font: size 5px;;
+}
+h1{font-family: glossy;
+font-style: oblique;
+}
+input{
+width: 30px;
+height: 30px;
+border-style: none;
+text-align: center;
+font-size: larger;
+
+}
+
+ input:focus {
+box-shadow: 0 0 0 0;
+outline: 0;
+}  
+sup{ color: black;
+
+font-size: small;}
+
+</style>
+
+<body>
+<h1>Palavra cruzada</h1> 
+<div class="palavracruzada">
+<div class="horizontal"><h2>Horizontal</h2>
+<p>1. Aquele que tem inveja  </p>
+
+<p>4.Individuo do sexo masculino </p>
+
+<p>5. o que é objeto da nossa mais alta aspiração: a solução perfeita </p>
+
+<p>7. Instrumento bastante antigo, composto de cordas estendidas numa moldura aberta  </p>
+
+<p>8.Mover-se ou sustentar-se no ar por meio de asas ou algum meio mecânico </p>
+
+    
+</div>
+<table>
+<tr>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td> <sup>6</sup><input type="text"   maxlength="1"> </td>
+</tr>
+<tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td> <sup>7</sup> <input type="text"   maxlength="1"></td>
+    <td><input type="text"  maxlength="1"></td>
+    <td> <input type="text" maxlength="1"></td>
+    <td> <input type="text" maxlength="1"></td>
+    <td><input type="text"  maxlength="1"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td><input type="text"   maxlength="1"></td>
+        </tr>
+        <tr>
+            <td><sup>8</sup> <input type="text"   maxlength="1"></td>
+            <td><input type="text"   maxlength="1"></td>
+            <td><input type="text"   maxlength="1"></td>
+            <td> <sup>2</sup><input type="text"   maxlength="1"></td>
+            <td></td>
+            <td> <sup>4</sup><input type="text"   maxlength="1"></td>
+            <td><input type="text"   maxlength="1"></td>
+            <td> <sup>3</sup><input type="text"   maxlength="1"></td>
+            <td><input type="text"   maxlength="1"></td>
+            <td><input type="text"   maxlength="1"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><input type="text"   maxlength="1"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><input type="text"   maxlength="1"></td>
+                <td></td>
+                <td><input type="text"   maxlength="1"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td> <sup>1</sup><input type="text"   maxlength="1"></td>
+                    <td><input type="text"   maxlength="1"></td>
+                    <td><input type="text"   maxlength="1"></td>
+                    <td > <input type="text"   maxlength="1"></td>
+                    <td > <input type="text"   maxlength="1"></td>
+                    <td ><input type="text"   maxlength="1"></td>
+                    <td ><input type="text"   maxlength="1"></td>
+                    <td ><input type="text"   maxlength="1"></td>
+                    <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td><input type="text"   maxlength="1"></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td> <input type="text"   maxlength="1"></td>
+                        <td></td>
+                        <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td> <sup>5</sup><input type="text"   maxlength="1"></td>
+                            <td><input type="text"   maxlength="1"></td>
+                            <td><input type="text"   maxlength="1"></td>
+                            <td><input type="text"   maxlength="1"></td>
+                            <td><input type="text"   maxlength="1"></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td><input type="text"   maxlength="1"></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><input type="text"   maxlength="1"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    </tr>
+</table>
+
+<div class="vertical"><h2>Vertical</h2>
+<p> 2.Vingar uma agressão com outra maior,mais violenta </p>
+
+<p>3. Móvel destinado a refeiçôes,jogos,apoio etc </p>
+
+<p>6. Quieto, sossegado: não apresenta agitação, pertubação </p>
+
+
+<button >verificar</button>
+
+</div>
+</div>
+</body>
